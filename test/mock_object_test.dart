e6ebe9bd7bb73cf91eea9ee51cda6d257898b5d2
Fardiansyah <fardiansyah@ifca.co.id>
2023-11-14 17:28:31 +0700

    end

--- test/mock_object_test.dart
+++ test/mock_object_test.dart
@@ -1,6 +1,7 @@
 import 'package:belajar_dart_unit_test/book.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 @GenerateNiceMocks([MockSpec<BookRepository>()])
@@ -13,7 +14,31 @@ void main() {
 
     test("New book is Saved", () {
       bookService.save("1", "Grand Theft Auto", 99000);
-      verify(bookRepository.save(Book("1", "Grand Theft Auto", 99000)));
+      verify(bookRepository.save(Book("1", "Grand Theft Auto", 99000)))
+          .called(1);
+    });
+
+    test("Find book by id is not found", () {
+      expect(() {
+        bookService.find("1");
+      }, throwsException);
+    });
+
+    test("Find book by id is success", () {
+      when(bookRepository.findById("1"))
+          .thenReturn(Book("1", "Grand Theft Auto", 99000));
+
+      var book = bookService.find("1");
+      expect(book, equals(Book("1", "Grand Theft Auto", 99000)));
+      verify(bookRepository.findById("1")).called(1);
+    });
+    test("Find book by id is fardi1998", () {
+      when(bookRepository.findById(argThat(startsWith("fardi"))))
+          .thenReturn(Book("fardi1998", "Grand Theft Auto", 99000));
+
+      var book = bookService.find("fardi1998");
+      expect(book, equals(Book("fardi1998", "Grand Theft Auto", 99000)));
+      verify(bookRepository.findById(any)).called(1);
     });
   });
 }
