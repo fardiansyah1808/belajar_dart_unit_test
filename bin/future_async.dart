609306e8a0277265b6ac9dcf1b125240f60e3ec3
fardiansyah1808 <fardiansyah@ifca.co.id>
2023-11-16 12:28:11 +0700

    coba

--- bin/future_async.dart
+++ bin/future_async.dart
@@ -1,10 +1,10 @@
 Future<void> hello() {
   return Future.delayed(Duration(seconds: 3), () {
-    print("Fardi Anjayy Ansyah");
+    print("Done!");
   });
 }
 
 void main() {
   hello();
-  print("Done");
+  print("Done gak bang?");
 }
